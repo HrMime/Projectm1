04eb371bc100f2280315b8a69f645afe5251ebcf
HrMime <112628522+HrMime@users.noreply.github.com>
2022-10-04 19:16:19 +0200

    df

--- app.py
+++ app.py
@@ -87,6 +87,6 @@ if st.button('Predict! 🚀'):
     st.metric(label="Predicted output", value=f'{round(predicted_value)}')
     
     #print SHAP explainer to user
-    st.subheader(f'Why {predicted_value}? See below:')
+    st.subheader(f'Why {predicted_value.inverse_transform(y)}? See below:')
     shap_value = explainer.shap_values(line_to_pred)
     st_shap(shap.force_plot(explainer.expected_value, shap_value, line_to_pred), height=400, width=500)
