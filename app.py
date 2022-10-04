c9bfc4d9447da73841f6dc96199512f31c7193c4
HrMime <112628522+HrMime@users.noreply.github.com>
2022-10-04 17:48:31 +0200

    df

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-import streamlit as st
+# Load packages (comments for more special stuff)
+
 import pandas as pd
+import pickle # un-pickling stuff from training notebook
+from xgboost import XGBRegressor # we use a trained XGBoost model...and therefore need to load it
+from sklearn.preprocessing import StandardScaler
+import shap # add prediction explainability
+
+import numpy as np
+import itertools # we need that to flatten ohe.categories_ into one list for columns
+import streamlit as st
+from streamlit_shap import st_shap # wrapper to display nice shap viz in the app
+
 st.markdown("hej")
