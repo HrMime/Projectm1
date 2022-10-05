3264b0fb376ddf163a83825ec8a3521c2f833375
HrMime <112628522+HrMime@users.noreply.github.com>
2022-10-05 12:14:07 +0200

    df

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ import itertools # we need that to flatten ohe.categories_ into one list for col
 import streamlit as st
 from streamlit_shap import st_shap # wrapper to display nice shap viz in the app
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import plotly.express as px
+
 ##Streamlit interface:
 st.set_page_config(page_title='Bank Marketing Project',
                     page_icon="🐙",
@@ -25,12 +29,13 @@ tab1, tab2, tab3 = st.tabs(["Predictor tool SML", "SML Model Comparison", "SML"]
 
 with tab1:
 
-    st.subheader('Will this given costumer say yes?')
+        st.title('Will this given costumer say yes?')
 
-    #this is how you can add images e.g. from unsplash (or loca image file)
-    #st.image('https://images.unsplash.com/photo-1577346895253-445f05a6670d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2NDY1MzMwNQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image('https://source.unsplash.com/WgUHuGSWPVM', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
-    # use this decorator (--> @st.experimental_singleton) and 0-parameters function to only load and preprocess once
+    data = pd.read_csv("https://raw.githubusercontent.com/Ceges98/BDS-Project/main/bank_marketing.csv", sep=";")
+    data = data[data["education"].str.contains("unknown") == False]
+
     @st.experimental_singleton
     def read_objects():
         model_xgb = pickle.load(open('model_xgb.pkl','rb'))
@@ -42,36 +47,55 @@ with tab1:
 
     model_xgb, scaler, ohe, cats, shap_values = read_objects()
 
-    # define explainer
+    #Explainer defined
     explainer = shap.TreeExplainer(model_xgb)
 
-    #write some markdown blah
-    with st.expander("What's that app?"):
+    with st.expander("What's the purpose of this app?"):
         st.markdown("""
-        This app will help you determine what you should be asking people to pay per night for staying at your awesome place.
-        We trained an AI on successful places in Copenhagen. It will give you a pricing suggestion given a few inputs.
-        We recommend going around 350kr up or down depending on the amenities that you can provide and the quality of your place.
-        As a little extra 🌟, we added an AI explainer 🤖 to understand factors driving prices up or down.
+        This app will help you determine if you should call a given costumer! 💵 💴 💶 💷
+        It can further help you reconsider your strategic approach to the costumer,
+        in the case that our SML model will predict a "No" from the costumer.
         """)
 
-    st.subheader('Costumer description')
+    st.title('Costumer description')
 
-    # here you collect all inputs from the user into new objects
-    job = st.selectbox('What is his/hers jobtype?', options=ohe.categories_[0])
-    marital = st.radio('Marital', options=ohe.categories_[1])
+    #Below all the bank client's info will be selected
+    st.subheader("Select the Customer's Age")
+    age = st.slider("", min_value = 17, max_value = 98, 
+                            step = 1, value = 41)
+    st.write("Selected Age:", age)
+
+    st.subheader("Select the Customer's Jobtype")
+    job = st.radio("", ohe.categories_[0])
+    st.write("Selected Job:", job)
+
+    st.subheader("Select the Customer's Marital")
+    marital = st.radio("", ohe.categories_[1])
+    st.write("Selected Marital:", marital)
+
+    st.subheader("Select the Customer's Education")
+    education = st.radio("", data['education'].unique())
+    st.write("Selected Education:", education)
+    #Defining a encoding function for education
+    def encode_education(selected_item):
+        dict_education = {'basic.4y':1, 'high.school':4, 'basic.6y':2, 'basic.9y':3, 'professional.course':5, 'university.degree':6, 
+    'illiterate':0}
+        return dict_education.get(selected_item)
+    ### Using function for encoding on education
+    education = encode_education(education) 
+
     poutcome = st.selectbox('What was the previous outcome for this costumer?', options=ohe.categories_[4])
-    age = st.number_input('Age?', min_value=17, max_value=98)
-    education = st.number_input('Education', min_value=0, max_value=7)
     campaign = st.number_input('How many contacts have you made for this costumer for this campagin already?', min_value=0, max_value=35)
     previous = st.number_input('How many times have you contacted this client before?', min_value=0, max_value=35)
 
-    # make a nice button that triggers creation of a new data-line in the format that the model expects and prediction
-    if st.button('Predict! 🚀'):
+    #Button for predicting the costumers answer
+    if st.button('Deposit Prediction 💵'):
+
         # make a DF for categories and transform with one-hot-encoder
         new_df_cat = pd.DataFrame({'job':job,
                     'marital':marital,
-                    'month': 'oct',
-                    'day_of_week':'fri',
+                    'month': 'oct', #This could be coded with a date.today().month function
+                    'day_of_week':'fri', #This could aswell be coded with a function
                     'poutcome':poutcome}, index=[0])
         new_values_cat = pd.DataFrame(ohe.transform(new_df_cat), columns = cats , index=[0])
 
@@ -80,27 +104,29 @@ with tab1:
                                 'education': education,
                                 'campaign': campaign,
                                 'previous': previous, 
-                                'emp.var.rate': 1.1,
-                                'cons.price.idx': 93.994,
-                                'cons.conf.idx': -36.4,
-                                'euribor3m': 4.857,
-                                'nr.employed': 5191.0
+                                'emp.var.rate': 1.1, #This could be scraped from a site like Statistics Portugal
+                                'cons.price.idx': 93.994, #This could be scraped from a site like Statistics Portugal
+                                'cons.conf.idx': -36.4, #This could be scraped from a site like Statistics Portugal
+                                'euribor3m': 4.857, #This could be scraped from a site like Statistics Portugal
+                                'nr.employed': 5191.0 #This could be scraped from a site like Statistics Portugal
                             }, index=[0])
         new_values_num = pd.DataFrame(scaler.transform(new_df_num), columns = new_df_num.columns, index=[0])  
         
-        #bring all columns together
+        #Bringing all columns together
         line_to_pred = pd.concat([new_values_num, new_values_cat], axis=1)
 
-        #run prediction for 1 new observation
+        #Run prediction for the new observation. Inputs to this given above
         predicted_value = model_xgb.predict(line_to_pred)[0]
-
-        #print out result to user
-        st.metric(label="Predicted answer", value=f'{predicted_value}')
         
-        #print SHAP explainer to user
-        st.subheader(f'Why {predicted_value}? See below:')
+        
+        #Printing the result
+        st.metric(label="Predicted answer", value=f'{predicted_value}')
+        st.subheader(f'Why {predicted_value}? 1 equals to yes, while 0 equals to no')
+
+        #Printing SHAP explainer
+        st.subheader(f'Lets explain why the model predicts the output above! See below for SHAP value:')
         shap_value = explainer.shap_values(line_to_pred)
-        st_shap(shap.force_plot(explainer.expected_value, shap_value, line_to_pred), height=400, width=500)
+        st_shap(shap.force_plot(explainer.expected_value, shap_value, line_to_pred), height=400, width=900)
 
     with tab2:
         st.subheader("SML Model Accuracy")
